dd17870e6cf614f672850e8b761e0a5c73f7a923
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2021-01-09 11:38:21 +0900

    remove unnecessary config

--- src/main/java/com/example/rediscache/RedisConfig.java
+++ src/main/java/com/example/rediscache/RedisConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
@@ -14,13 +13,6 @@ public class RedisConfig {
     @Bean
     public JedisConnectionFactory redisConnectionFactory() {
         return new JedisConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        return template;
     }
 
     @Bean
