cca135c312165df7c947daf61cfa8d87692a7268
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2021-03-03 16:50:11 +0900

    set ttl

--- src/main/java/com/example/rediscache/RedisConfig.java
+++ src/main/java/com/example/rediscache/RedisConfig.java
@@ -1,11 +1,15 @@
 package com.example.rediscache;
 
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -18,5 +22,12 @@ public class RedisConfig {
     @Bean
     CacheManager redisCache(RedisConnectionFactory redisConnectionFactory) {
         return RedisCacheManager.builder(redisConnectionFactory).build();
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer myRedisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("query-by-id",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(10)));
     }
 }
